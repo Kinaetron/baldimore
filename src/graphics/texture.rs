3386b267734b0af4497cc918ef97f2ecdc47e75b
Darren Daley <darren.daley@hotmail.co.uk>
2022-07-30 15:52:23 +0100

    finished it off

--- src/graphics/texture.rs
+++ src/graphics/texture.rs
@@ -1,7 +1,8 @@
 use std::fs;
 use std::path::Path;
+use cgmath::Vector2;
 use std::num::NonZeroU32;
-use image::GenericImageView;
+use image::{GenericImageView, ImageBuffer, Rgba};
 
 use crate::platform::graphics_interface::GraphicsInterface;
 
@@ -17,7 +18,56 @@ pub struct Texture
 
 impl Texture 
 {
-    pub fn new(graphics_interface: &GraphicsInterface, file_path: &str) -> Self
+    pub fn new_from_buffer(graphics_interface: &GraphicsInterface, buffer: ImageBuffer<Rgba<u8>, Vec<u8>>, dimensions: Vector2<u32>) -> Self
+    {
+        let size = wgpu::Extent3d {
+            width: dimensions.x,
+            height: dimensions.y,
+            depth_or_array_layers: 1,
+        };
+
+        let texture = graphics_interface.device.create_texture(&wgpu::TextureDescriptor {
+            label: Some("rectangle"),
+            size,
+            mip_level_count: 1,
+            sample_count: 1,
+            dimension: wgpu::TextureDimension::D2,
+            format: wgpu::TextureFormat::Rgba8UnormSrgb,
+            usage: wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::COPY_DST,
+        });
+
+        graphics_interface.queue.write_texture(
+            wgpu::ImageCopyTexture {
+                aspect: wgpu::TextureAspect::All,
+                texture: &texture,
+                mip_level: 0,
+                origin: wgpu::Origin3d::ZERO,
+            },
+            &buffer,
+            wgpu::ImageDataLayout {
+                offset: 0,
+                bytes_per_row: NonZeroU32::new(4 * dimensions.x),
+                rows_per_image: NonZeroU32::new(dimensions.y),
+            },
+            size,
+        );
+
+        let view = texture.create_view(&wgpu::TextureViewDescriptor::default());
+        let sampler = graphics_interface.device.create_sampler(&wgpu::SamplerDescriptor {
+            address_mode_u: wgpu::AddressMode::ClampToEdge,
+            address_mode_v: wgpu::AddressMode::ClampToEdge,
+            address_mode_w: wgpu::AddressMode::ClampToEdge,
+            mag_filter: wgpu::FilterMode::Linear,
+            min_filter: wgpu::FilterMode::Nearest,
+            mipmap_filter: wgpu::FilterMode::Nearest,
+            ..Default::default()
+        });
+
+        Self { width: dimensions.x, height: dimensions.y, texture, view, sampler }
+    }
+
+
+    pub fn new_from_file(graphics_interface: &GraphicsInterface, file_path: &str) -> Self
     {
        
         let path = Path::new(file_path);
@@ -35,7 +85,7 @@ impl Texture
         };
 
         let texture = graphics_interface.device.create_texture(&wgpu::TextureDescriptor {
-            label: path.file_name().unwrap().to_str(),
+            label: Some("rectangle"),
             size,
             mip_level_count: 1,
             sample_count: 1,
@@ -71,6 +121,8 @@ impl Texture
             ..Default::default()
         });
 
-        Self { width: dimensions.0, height: dimensions.1, texture, view, sampler }
+        Self { width: dimensions.0, height: dimensions.1 , texture, view, sampler }
     }
+
+
 }
