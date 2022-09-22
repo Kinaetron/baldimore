58cb21064b0731ea57596b8a5ff879dfca7dc81d
Darren Daley <darren.daley@hotmail.co.uk>
2022-09-22 04:56:22 +0100

    finished up static circle vs rectangle collision

--- src/collision.rs
+++ src/collision.rs
@@ -1,3 +1,5 @@
+use cgmath::Zero;
+
 use crate::{math::Vector2, math::InnerSpace};
 use crate::shapes::{rectangle::Rectangle, circle::Circle};
 
@@ -22,7 +24,7 @@ pub fn rectangle_intersects_rectangle(rectangle_a: &Rectangle, rectangle_b: &Rec
     if distance_x.abs() >= min_distance_x || distance_y.abs() >= min_distance_y {
         return None;
     }
-    
+
     let depth_x = if distance_x > 0.0 { min_distance_x - distance_x } else { -min_distance_x - distance_x };
     let depth_y = if distance_y > 0.0 {  min_distance_y - distance_y } else { -min_distance_y - distance_y };
 
@@ -46,4 +48,96 @@ pub fn circle_intersects_circle(circle_a: &Circle, circle_b: &Circle) -> Option<
     let depth_vector = direction * depth;
 
     Some(depth_vector)
+}
+
+pub fn circle_intersects_rectangle(circle: &Circle, rectangle: &Rectangle) -> Option<Vector2<f32>>
+{
+    let mut vertices: Vec<Vector2<f32>> = Vec::new();
+
+    vertices.push(Vector2::new(rectangle.left, rectangle.top));
+    vertices.push(Vector2::new(rectangle.right, rectangle.top));
+    vertices.push(Vector2::new(rectangle.right, rectangle.bottom));
+    vertices.push(Vector2::new(rectangle.left, rectangle.bottom));
+
+    let mut is_outside = false;
+    let mut min_current_vertex: Vector2<f32> = Vector2::zero();
+    let mut min_next_vertex: Vector2<f32> = Vector2::zero();
+    let mut distance_circle_edge = f32::MIN;
+    let mut max_projection = f32::MIN;
+
+    for i in 0.. vertices.len()
+    {
+        let current_vertex = i;
+        let next_vertex = (i + 1) % vertices.len();
+
+        let edge = vertices[next_vertex] - vertices[current_vertex];
+        let normal = normal(&edge);
+
+        let vertex_to_circle_centre = circle.centre - vertices[current_vertex];
+        let projection = vertex_to_circle_centre.dot(normal);
+
+        if projection > 0.0 && projection > max_projection
+        {
+            max_projection = projection;
+            distance_circle_edge = projection;
+            min_current_vertex = vertices[current_vertex];
+            min_next_vertex = vertices[next_vertex];
+            is_outside = true;
+        }
+        else if projection > distance_circle_edge
+        {
+            distance_circle_edge = projection;
+            min_current_vertex = vertices[current_vertex];
+            min_next_vertex = vertices[next_vertex];
+        }
+    }
+
+    if is_outside
+    {
+        let mut v1 = circle.centre - min_current_vertex;
+        let mut v2  = min_next_vertex - min_next_vertex;
+
+        if v1.dot(v2) < 0.0
+        {
+            if v1.magnitude() > circle.radius {
+                return None;
+            }
+            else {
+                return Some(v1.normalize() * (circle.radius - v1.magnitude()));
+            }
+        }
+        else
+        {
+            v1 = circle.centre - min_next_vertex;
+            v2 = min_current_vertex - min_next_vertex;
+
+            if v1.dot(v2) < 0.0
+            {
+                if v1.magnitude() > circle.radius {
+                    return None;
+                }
+                else {
+                    return Some(v1.normalize() * (circle.radius - v1.magnitude()));
+                }
+            }
+            else 
+            {
+                if distance_circle_edge > circle.radius {
+                    return None;
+                }
+                else 
+                {
+                    let normal = normal(&(min_next_vertex - min_current_vertex));
+                    return Some(normal * (circle.radius - distance_circle_edge));
+                }
+            }
+        }
+    }
+
+    let normal = normal(&(min_next_vertex - min_current_vertex));
+    return Some(normal * (circle.radius - distance_circle_edge));
+}
+
+fn normal(vector: &Vector2<f32>) -> Vector2<f32> {
+    Vector2::new(vector.y, -vector.x).normalize()
 }
